823d4870c393ce0ed48e1731862a85e3b6d30266
Vaishupatil16 <156436514+Vaishupatil16@users.noreply.github.com>
2026-01-28 13:17:39 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,21 @@ import express from "express";
 import cors from "cors";
 import productRoutes from "./routes/products";
 import orderRoutes from "./routes/orders";
+import client from "prom-client";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Prometheus metrics
+client.collectDefaultMetrics();
+const register = client.register;
+
+app.get("/metrics", async (req, res) => {
+  res.setHeader("Content-Type", register.contentType);
+  res.end(await register.metrics());
+});
 
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
